a33a52b4d1f1f70b92328d57279aafc033848f6c
JoeSaranbro <108979915+JoeSaranbro@users.noreply.github.com>
2022-11-04 19:43:00 +0700

    k

--- src/Login.js
+++ src/Login.js
@@ -19,7 +19,7 @@ const Login = ( {setUser} ) => {
 
 
   return (
-    <div className='flex justify-center absolute top-1/2 left-1/2 -translate-x-1/2 bg-red-800'>
+    <div className='flex justify-center absolute top-1/2 left-1/2 -translate-x-1/2 bg-red-700'>
     <section className='section'>
       <form className='form' onSubmit={handleSubmit}>
         <h5>login</h5>
