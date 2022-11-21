44cb92c32ae2d1e3fdf8ab61bf17019e74a4be8d
JoeSaranbro <108979915+JoeSaranbro@users.noreply.github.com>
2022-11-21 21:17:04 +0700

    pass

---
+++ src/ForgotPassword.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ForgotPassword = () => {
+  return (
+    <div className='text-white'>ForgotPassword</div>
+  )
+}
+
+export default ForgotPassword
--- src/Login.js
+++ src/Login.js
@@ -1,25 +1,169 @@
-import { useState ,useRef, useEffect} from 'react';
+import { useState ,useRef, useEffect, useContext } from 'react';
+import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 
-const Login = ( {setUser} ) => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [isModalOpen, setModal] = useState(false);
-  const navigate = useNavigate()
+const Login = () => {
 
-    const ref = useRef();
+  const userRef = useRef();
+  const errRef = useRef();
+  const ref = useRef();
+
+  const [userName, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [isSignup, setIsSignup] = useState(false);
+  const [isModalOpen, setModal] = useState(false);
+  
+  
+  console.log("signup =  " +isSignup)
+  
+  useEffect(() => {
+    userRef.current.focus();
+}, [])
+
+  useEffect(()=> {
+    setErrMsg('')
+  }, [userName, pwd]);
+
+  
+ 
+  
+  
+
+  const LoginForm = () => {
+      
+      return (
+      <div id="loginModal" className="modal" style={isModalOpen === true ? {display: "block"}:{display: "none"}}>
+        <div ref={ref} className="modal-content-login ">
+          <div id="header" className='text-center'>
+          <span className='text-2xl font-bold'>Login<span className="close rounded-lg" onClick={(e)=> {setModal(false)}}>&times;</span></span>
+          
+          <hr className='mt-4 border-2'/>
+          </div>
+          
+          <div className=''>
+          <form className='form'>
+            <div className='form-col'>
+              <label htmlFor='username' className='form-label'>
+                Username/Email
+              </label>
+              <input
+                type='text'
+                id='username'
+                className='form-input'
+                ref={userRef}
+                value={userName}
+                onChange={(e) => setUser(e.target.value)}
+              />
+            </div>
+            <div className='form-col'>
+              <label htmlFor='password' className='form-label'>
+                Password
+              </label>
+              <input
+                type='password'
+                className='form-input'
+                id='password'
+                value={pwd}
+                onChange={(e) => setPwd(e.target.value)}
+              />
+              <div className="mt-2 text-base text-blue-600 hover:text-blue-800 hover:underline"><Link to="/portfolio">Forgot Password?</Link></div>
+            </div>
+            <div className='mt-4'>
+            <button type='submit' className='form-button-login'>
+              <p>Login</p>
+            </button>
+            <button type="button" onClick={()=> {setIsSignup(true)}} className='ml-4 form-button-signup'>
+            <p>Sign up</p>
+            </button>
+            </div>
+            
+        </form>
+          </div>
+        </div>
+    </div>
+    )}
+  
+    const SignUpForm = () => {
+      return (
+      <div id="signupModal" className="modal" style={isModalOpen === true ? {display: "block"}:{display: "none"}}>
+        <div ref={ref} className="modal-content-register ">
+          <div id="header" className='text-center'>
+          <span className='text-2xl font-bold'>Sign up<span className="close rounded-lg" onClick={(e)=> {(setModal(false))(setIsSignup(false))}}>&times;</span></span>
+          
+          <hr className='mt-4 border-2'/>
+          </div>
+          
+          <div className=''>
+          <form className='form'>
+            <div className='form-col'>
+              <label htmlFor='username' className='form-label'>
+                Username
+              </label>
+              <input
+                type='text'
+                id='username'
+                className='form-input'
+                ref={userRef}
+                value={userName}
+                onChange={(e) => setUser(e.target.value)}
+              />
+            </div>
+            <div className='form-col'>
+              <label htmlFor='email' className='form-label'>
+                Email
+              </label>
+              <input
+                type='email'
+                id='email'
+                className='form-input'
+                ref={userRef}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className='form-col'>
+              <label htmlFor='password' className='form-label'>
+                Password
+              </label>
+              <input
+                type='password'
+                className='form-input'
+                id='password'
+                value={pwd}
+                onChange={(e) => setPwd(e.target.value)}
+              />
+            </div>
+            <div className='mt-4'>
+            <button onClick={()=> {setIsSignup(false)}} type='submit' className='form-button-login'>
+              <p>Back to Login</p>
+            </button>
+            <button onClick={(e)=> {e.preventDefault()}} className='ml-4 form-button-signup'>
+            <p>Sign up</p>
+            </button>
+            </div>
+            
+        </form>
+          </div>
+        </div>
+    </div>
+    )}
   
     const handleEsc = (event) => {
       if (event.key === "Escape") {
         document.getElementById("openmodal").blur();
         console.log(document.getElementById("openmodal"))
         setModal(false);
+        setIsSignup(false);
       }
     };
   
     const handleClickOutside = (event) => {
       if (!ref.current.contains(event.target)) {
         setModal(false);
+        setIsSignup(false);
        // console.log(ref)
         
       }
@@ -40,65 +184,25 @@ const Login = ( {setUser} ) => {
   
   
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(!name);
-    if(!name || !email) return;
-    setUser({name: name, email: email});
-    navigate('/dashboard')
-  };
   
-  const handleLogin = () =>{
-  }
+  
+  
  
    
 
   return (
     <div className='h-screen'>
       <div>
-      <button id="openmodal" onClick={()=> {setModal(true)}} className="btnGray">Login</button>
-        <div id="myModal" className="modal" style={isModalOpen === true ? {display: "block"}:{display: "none"}}>
-          <div ref={ref} className="modal-content ">
-            <div id="header" className='text-center'>
-            <span className='text-2xl'>Login to Website</span>
-            <span className="close rounded-lg" onClick={(e)=> {setModal(false)}}>&times;</span>
-            </div>
-            
-            <div>
-            <form className='form' onSubmit={handleSubmit}>
-              <div className='form-row'>
-                <label htmlFor='name' className='form-label'>
-                  name
-                </label>
-                <input
-                  type='text'
-                  className='form-input'
-                  id='name'
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </div>
-              <div className='form-row'>
-                <label htmlFor='email' className='form-label'>
-                  email
-                </label>
-                <input
-                  type='email'
-                  className='form-input'
-                  id='email'
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <button type='submit' className='btn btn-block'>
-                login
-              </button>
-           </form>
-            </div>
-          </div>
-        </div>
+      <button id="openmodal" onClick={()=> setModal(true)} className="btnGray">Login</button>
+      {isSignup ? <SignUpForm/>:<LoginForm/>}
+      
+      
+      
+      
+        
         </div>
       <h2>1111111111111</h2>
+      
     </div>
   );
 };
